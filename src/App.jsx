52df16a920cb323ff2944d992904c780c82ae50d
3ddyBoi <70267476+3ddyBoi@users.noreply.github.com>
2022-01-16 17:23:16 +0100

    Added nl-ui
    nl-ui is a npm package that makes react-helmet even more simple.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { DefaultHelmet } from 'nl-ui';
 import './App.css';
 
 const App = () => {
     return (
         <div className='App'>
+            <DefaultHelmet Title='Portfolio' SubTitle='Homepage' />
             <h1>Portfolio</h1>
         </div>
     );
