a3800bc92d880fadd0ed6bd934b0c744c979d556
3ddyBoi <70267476+3ddyBoi@users.noreply.github.com>
2022-01-16 22:33:42 +0100

    Moved BrowserRouter to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { DefaultHelmet } from 'nl-ui';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/Navigation';
+
+import Home from './pages/Home';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ProjectsPage from './pages/ProjectsPage';
 
 const App = () => {
     return (
         <div className='App'>
-            <DefaultHelmet Title='Portfolio' SubTitle='Homepage' />
-            <Navigation />
-            <h1>Portfolio</h1>
+            <DefaultHelmet Title='Portfolio' />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<AboutPage />} />
+                <Route path='/projects' element={<ProjectsPage />} />
+                <Route path='/contact' element={<ContactPage />} />
+            </Routes>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AboutPage from './pages/AboutPage';
-import ProjectsPage from './pages/ProjectsPage';
-import ContactPage from './pages/ContactPage';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/about' element={<AboutPage />} />
-                <Route path='/projects' element={<ProjectsPage />} />
-                <Route path='/contact' element={<ContactPage />} />
-            </Routes>
+            <App />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
