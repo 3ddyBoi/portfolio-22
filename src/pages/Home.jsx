29a8881e5da11a351e716e74e1326df09847e231
3ddyBoi <70267476+3ddyBoi@users.noreply.github.com>
2022-01-16 22:33:12 +0100

    Added homePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { DefaultHelmet } from 'nl-ui';
+import Navigation from '../components/Navigation';
+
+import romsdalseggen1 from '../assets/images/romsdalseggen-1.jpeg';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <DefaultHelmet Title='Portfolio' SubTitle='Homepage' />
+            <section id='home'>
+                <div className='background-img'>
+                    <img src={romsdalseggen1} alt='Backgroundimage' />
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
