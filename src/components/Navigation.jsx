ddb53bb5a04f4e8eaffd49ad5443792abd1efb8b
3ddyBoi <70267476+3ddyBoi@users.noreply.github.com>
2022-01-16 22:33:47 +0100

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,34 @@
 import { Link } from 'react-router-dom';
 
 const navigation = () => {
+    const scrollToElement = (id) => {
+        const element = document.getElementById(id);
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    };
+
     return (
-        <nav className='menu'>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                    <Link to='/about'>About</Link>
-                    <Link to='/projects'>Projects</Link>
-                    <Link to='/contact'>Contact</Link>
-                </li>
-            </ul>
+        <nav className='navigation'>
+            <div className='navigation-content'>
+                <div className='navigation-logo'>
+                    <h2>
+                        <Link to='/'>Portfolio</Link>
+                    </h2>
+                </div>
+                <div className='navigation-items'>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/projects'>Projects</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </div>
+            </div>
         </nav>
     );
 };
