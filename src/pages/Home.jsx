e258f40fc20b0158491e00989eb46671b5056d19
3ddyBoi <70267476+3ddyBoi@users.noreply.github.com>
2022-01-17 20:26:36 +0100

    Added some styling to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { DefaultHelmet } from 'nl-ui';
 import Navigation from '../components/Navigation';
 
+import './Home.css';
+
 import romsdalseggen1 from '../assets/images/romsdalseggen-1.jpeg';
 
 const Home = () => {
