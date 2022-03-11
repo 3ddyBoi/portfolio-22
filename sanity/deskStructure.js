932ed345075c06b6446c80212d03d559eb1097e5
Edvard Høiby <EdvardHoiby@outlook.com>
2022-03-11 13:19:43 +0100

    Deskstructure change

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -1,5 +1,6 @@
 import S from '@sanity/desk-tool/structure-builder';
 import { GoSettings } from 'react-icons/go';
+import { MdMenu } from 'react-icons/md';
 
 export default () =>
     S.list()
@@ -14,5 +15,38 @@ export default () =>
                         .schemaType('siteSettings')
                         .documentId('siteSettings')
                 ),
-            // S.documentListItem().schemaType('').title('Frontpage'),
+            S.documentListItem()
+                .title('Page Builder')
+                .child(
+                    S.list()
+                        .title('Landing Pages')
+                        .items([
+                            S.listItem()
+                                .title('Navigation Menus')
+                                .icon(MdMenu)
+                                .schemaType('navMenu')
+                                .child(
+                                    S.documentTypeList(
+                                        'navMenu'
+                                    ).title('Navigation Menus')
+                                ),
+                            S.listItem()
+                                .title('Routes')
+                                .schemaType('route')
+                                .child(
+                                    S.documentTypeList('route')
+                                        .title('Routes')
+                                        .child((documentId) =>
+                                            S.document()
+                                                .documentId(
+                                                    documentId
+                                                )
+                                                .schemaType('route')
+                                                .views([
+                                                    S.view.form(),
+                                                ])
+                                        )
+                                ),
+                        ])
+                ),
         ]);
