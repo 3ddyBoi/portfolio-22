1426a7255382fbb3aa94bdb1d580ad06466398c7
Edvard Høiby <EdvardHoiby@outlook.com>
2022-03-21 16:36:43 +0100

    Started with responsive design and finished footer

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import '../scss/Footer.scss';
+
+import { ReactComponent as FooterWave } from '../assets/svg/footer-wave.svg';
+
 import { AiFillInstagram } from 'react-icons/ai';
 import { FaTwitch, FaYoutube } from 'react-icons/fa';
 import { BsLinkedin } from 'react-icons/bs';
@@ -7,21 +11,53 @@ import { BsLinkedin } from 'react-icons/bs';
 const Footer = () => {
     return (
         <footer id='footer' className='footer'>
+            <FooterWave className='footer-wave' />
             <div className='footer-content'>
                 <div className='footer-content-socials'>
-                    <AiFillInstagram />
-                    <FaTwitch />
-                    <FaYoutube />
-                    <BsLinkedin />
+                    <a
+                        className='footer-content-socials-icon'
+                        href='https://www.instagram.com/marcusflaterudhagen0/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <AiFillInstagram />
+                    </a>
+                    <a
+                        className='footer-content-socials-icon'
+                        href='https://www.twitch.tv/murkesrl'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <FaTwitch />
+                    </a>
+                    <a
+                        className='footer-content-socials-icon'
+                        href='https://www.youtube.com/c/MarcusFlaterudhagen/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <FaYoutube />
+                    </a>
+                    <a
+                        className='footer-content-socials-icon'
+                        href='https://www.linkedin.com/in/marcusflaterudhagen/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <BsLinkedin />
+                    </a>
                 </div>
                 <div className='footer-content-contact'>
                     <h3>Marcus Flaterudhagen</h3>
-                    <a href='mailto:example@mail.com'>
-                        example@mail.com
-                    </a>
-                    <a href='tel:+4747654146'>+47 476 54 146</a>
+                    <div className='footer-content-contact-types'>
+                        <a href='mailto:example@mail.com'>
+                            example@mail.com
+                        </a>
+                        <span></span>
+                        <a href='tel:+4747654146'>+47 476 54 146</a>
+                    </div>
                 </div>
-                <div className='plugs'>
+                <div className='footer-content-plugs'>
                     <h3>Developed by Edvard Høiby</h3>
                     <h3>Designed by Marcus Flaterudhagen</h3>
                 </div>
